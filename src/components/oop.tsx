ccf526294db217f1ffe39ccf37710bb1d041b827
Dusuna <dusunax@gmail.com>
2022-11-11 21:19:09 +0900

    study oop with ts

--- src/components/oop.tsx
+++ src/components/oop.tsx
@@ -1,10 +1,12 @@
-import { accounting, accountingCopy } from "../util/OOP";
+import { accounting, IT } from "../util/OOP";
 
 export default function OOP() {
-  console.log("아래는 accounting");
-  console.log(accounting);
-  console.log("아래는 accountingCopy");
-  console.log(accountingCopy);
+  // console.log("아래는 accounting");
+  // console.log(accounting);
+  // console.log("아래는 accountingCopy");
+  // console.log(accountingCopy);
+  console.log(IT);
+  // console.log(accounting.mostRecentReport);
 
   return <p>OOP</p>;
 }
--- src/util/OOP.ts
+++ src/util/OOP.ts
@@ -1,21 +1,82 @@
 class Department {
-  name: string;
+  static readonly fiscalYear = 2022;
+  static hello = Department.fiscalYear - 22;
+  protected employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {}
+
+  static createEmployee(name: string) {
+    return { name: name, hello: Department.hello };
   }
 
   describe(this: Department) {
     console.log(`Department: ${this.name}`);
   }
+
+  addEmployee(employees: string) {
+    this.employees.push(employees);
+  }
+
+  printEmployeed() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+class ITDepartement extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
 }
 
-const accounting = new Department("Accounting");
+class AccountingDepartment extends Department {
+  private lastReport: string;
 
-accounting.describe();
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("report가 없습니다.");
+  }
 
-const accountingCopy = { name: "hello", describe: accounting.describe };
+  set mostRecentReport(value: string) {
+    if (!value || value === "") {
+      throw new Error("값을 입력해주세요");
+    }
+    this.addReport(value);
+  }
 
-accountingCopy.describe();
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
 
-export { accounting, accountingCopy };
+  addEmployee(name: string) {
+    if (name === "kim") return;
+
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReport() {
+    console.log(this.reports);
+  }
+}
+
+// 여기부터 인스턴스
+const IT = new ITDepartement("0124", ["Jack"]);
+IT.addEmployee("Micheal");
+
+const accounting = new AccountingDepartment("0125", []);
+
+accounting.addReport("hello");
+
+// console.log((accounting.mostRecentReport = "hi"));
+// console.log(accounting.mostRecentReport);
+
+console.log(AccountingDepartment.fiscalYear);
+console.log(Department.createEmployee("hi"));
+
+export { accounting, IT };
